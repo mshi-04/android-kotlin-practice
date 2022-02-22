0aecf76cfac8f8aaebdbc3e793f0dbe7e950b3bf
masato93426 <94366044+masato93426@users.noreply.github.com>
2022-02-22 21:16:44 +0900

    first commit

---
+++ ImplementationTest/app/src/main/java/com/example/implementationtest/activity/WebViewActivity.kt
@@ -0,0 +1,20 @@
+package com.example.implementationtest.activity
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.implementationtest.R
+
+class WebViewActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_webview)
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://www.yahoo.co.jp")
+    }
+}
